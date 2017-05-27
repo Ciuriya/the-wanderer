c00fb4c730de41402f5c5521b0e34a56a0ff7545
alexcomo95 <alexguitarmaster95@hotmail.fr>
2017-05-27 14:21:16 -0400

    Auto stash before merge of "MaxL" and "origin/MaxL"

--- Intercollégial Wanderers/Assets/Scripts/General/PlayerController.cs
+++ Intercollégial Wanderers/Assets/Scripts/General/PlayerController.cs
@@ -2,56 +2,137 @@
 using System;
 using System.Collections.Generic;
 using UnityEngine.SceneManagement;
+using System.Collections;
 
 namespace Player2D
 {
-    [RequireComponent(typeof(PlayerStats), typeof(BoxCollider2D), typeof(Rigidbody2D))]
+    [RequireComponent(typeof(PlayerStats))]
     public class PlayerController : MonoBehaviour
     {
-        public float m_acceleration;    // Acceleration of the player
-        public float m_moveSpeed;       // Max movement speed
-        public float m_jumpHeight;      // Speed of the jump
+        [HideInInspector]
+        public bool facingRight = true;         // For determining which way the player is currently facing.
+        [HideInInspector]
+        public bool jump = false;               // Condition for whether the player should jump.
+
+
+        public float moveForce = 365f;          // Amount of force added to move the player left and right.
+        public float maxSpeed = 5f;             // The fastest the player can travel in the x axis.
+        public AudioClip[] jumpClips;           // Array of clips for when the player jumps.
+        public float jumpForce = 1000f;         // Amount of force added when the player jumps.
+        public AudioClip[] taunts;              // Array of clips for when the player taunts.
+        public float tauntProbability = 50f;    // Chance of a taunt happening.
+        public float tauntDelay = 1f;           // Delay for when the taunt should happen.
+
+
+        private int tauntIndex;                 // The index of the taunts array indicating the most recent taunt.
+        private Transform groundCheck;          // A position marking where to check if the player is grounded.
+        private bool grounded = false;          // Whether or not the player is grounded.
+        private Animator anim;                  // Reference to the player's animator component.
+
+
+        void Awake()
+        {
+            // Setting up references.
+            groundCheck = transform.Find("groundChecker");
+            //anim = GetComponent<Animator>();
+        }
+
 
         void Update()
         {
-            if (GetComponent<PlayerStats>().getCurrentLife() <= 0) {
-                Application.LoadLevel(0);
-            }
+            // The player is grounded if a linecast to the groundcheck position hits anything on the ground layer.
+            grounded = Physics2D.Linecast(transform.position, groundCheck.position, 1 << LayerMask.NameToLayer("Ground"));
 
-            Rigidbody2D rigidbody = GetComponent<Rigidbody2D>();
+            // If the jump button is pressed and the player is grounded then the player should jump.
+            if (Input.GetButtonDown("Jump") && grounded)
+                jump = true;
+        }
 
-            if (Input.GetKeyDown(KeyCode.Space))
+
+        void FixedUpdate()
+        {
+            // Cache the horizontal input.
+            float h = Input.GetAxis("Horizontal");
+
+            // The Speed animator parameter is set to the absolute value of the horizontal input.
+            //anim.SetFloat("Speed", Mathf.Abs(h));
+
+            // If the player is changing direction (h has a different sign to velocity.x) or hasn't reached maxSpeed yet...
+            if (GetComponent<Rigidbody2D>().velocity.x < maxSpeed)
+                // ... add a force to the player.
+                GetComponent<Rigidbody2D>().AddForce(Vector2.right * moveForce);
+
+            // If the player's horizontal velocity is greater than the maxSpeed...
+            if (Mathf.Abs(GetComponent<Rigidbody2D>().velocity.x) > maxSpeed)
+                // ... set the player's velocity to the maxSpeed in the x axis.
+                GetComponent<Rigidbody2D>().velocity = new Vector2(Mathf.Sign(GetComponent<Rigidbody2D>().velocity.x) * maxSpeed, GetComponent<Rigidbody2D>().velocity.y);
+            
+            // If the player should jump...
+            if (jump)
             {
-                rigidbody.AddRelativeForce(new Vector2(0, m_jumpHeight), ForceMode2D.Impulse);
-            }
+                // Set the Jump animator trigger parameter.
+                //anim.SetTrigger("Jump");
 
-            rigidbody.AddRelativeForce(new Vector2(m_acceleration, 0), ForceMode2D.Force);
+                // Play a random jump audio clip.
+                int i = UnityEngine.Random.Range(0, jumpClips.Length);
+                AudioSource.PlayClipAtPoint(jumpClips[i], transform.position);
 
-            if (rigidbody.velocity.x > m_moveSpeed)
-            {
-                rigidbody.velocity = new Vector2(rigidbody.velocity.normalized.x * m_moveSpeed, rigidbody.velocity.y);
+                // Add a vertical force to the player.
+                GetComponent<Rigidbody2D>().AddForce(new Vector2(0f, jumpForce));
+
+                // Make sure the player can't jump again until the jump conditions from Update are satisfied.
+                jump = false;
             }
         }
 
-        void OnCollisionEnter2D(Collision2D collision)
+
+        void Flip()
         {
-            Collider2D collider = collision.collider;
-            PlayerStats stats = GetComponent<PlayerStats>();
+            // Switch the way the player is labelled as facing.
+            facingRight = !facingRight;
 
-            // If we touch an ennemy
-            if (collider.tag == "Ennemy")
+            // Multiply the player's x local scale by -1.
+            Vector3 theScale = transform.localScale;
+            theScale.x *= -1;
+            transform.localScale = theScale;
+        }
+
+
+        public IEnumerator Taunt()
+        {
+            // Check the random chance of taunting.
+            float tauntChance = UnityEngine.Random.Range(0f, 100f);
+            if (tauntChance > tauntProbability)
             {
-                stats.damage(1);
-            }
+                // Wait for tauntDelay number of seconds.
+                yield return new WaitForSeconds(tauntDelay);
 
-            // If we collide from the side to an object
-            Vector3 contactPoint = collision.contacts[0].point;
-            Vector3 center = collider.bounds.center;
+                // If there is no clip currently playing.
+                if (!GetComponent<AudioSource>().isPlaying)
+                {
+                    // Choose a random, but different taunt.
+                    tauntIndex = TauntRandom();
 
-            if (contactPoint.x > center.x)
-            {
-                stats.damage(1);
+                    // Play the new taunt.
+                    GetComponent<AudioSource>().clip = taunts[tauntIndex];
+                    GetComponent<AudioSource>().Play();
+                }
             }
+        }
+
+
+        int TauntRandom()
+        {
+            // Choose a random index of the taunts array.
+            int i = UnityEngine.Random.Range(0, taunts.Length);
+
+            // If it's the same as the previous taunt...
+            if (i == tauntIndex)
+                // ... try another random taunt.
+                return TauntRandom();
+            else
+                // Otherwise return this index.
+                return i;
         }
     }
 }
